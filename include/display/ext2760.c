b20da9cee16521b77dbbb4929051ceab8e6c95a3
Andrey Bykanov <shodan@xn--h1aeegel.net>
2016-03-10 19:31:36 +0300

    Добавлена индикация режима DFU и анализ бекап-регистра.

--- include/display/ext2760.c
+++ include/display/ext2760.c
@@ -28,6 +28,7 @@ void display_on()               // Инициализация порта LCD дисплея
 {
   GPIO_InitTypeDef GPIO_InitStructure;
 
+
   Power.Display_active = ENABLE;
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
